2f90e50ffb949b89545430bd7f8ee37dadcba143
vikashh20-3 <vvikashrao203@gmail.com>
2023-07-10 18:05:24 +0530

    fs loop 18:05 10Jul

--- filesystem.js
+++ filesystem.js
@@ -1,4 +1,17 @@
 const fs = require("fs"); //this is non global module so we have to import this
-fs.writeFileSync("demofile.txt", "This file is created by file system module ");
+// fs.writeFileSync("demofile.txt", "This file is created by file system module ");
 console.log("Directory name", __dirname); //to check the directory
 console.log("File Name", __filename); //to show the file name
+
+// Create files in a folder using loop
+
+const path = require("path");
+const dirPath = path.join(__dirname, "fsfiles"); //It Provides us the path for our folders
+
+fs.writeFileSync(dirPath + "/file.txt", "demo for folder");
+
+//file  creation with loop
+
+for (i = 2; i < 5; i++) {
+  fs.writeFileSync(dirPath + `/demofiles${i}`, "demo text");
+}
