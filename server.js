9a4efdd4cba9158859490e19dfc7469addb7d253
vikashh20-3 <vvikashrao203@gmail.com>
2023-07-10 15:22:30 +0530

    15:22 10jul

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
 const http = require("http");
-http.createServer().listen(4500);
+
+// first method
+http
+  .createServer((req, resp) => {
+    resp.write("hi");
+    resp.end();
+  })
+  .listen(4500);
+
+// Second Method
+http.createServer(dataControll).listen(4500);
+
+//simple function
+function dataControll(req, resp) {
+  resp.write("This is Data Controller Function");
+  resp.end();
+}
+
+// Arrow function
+const dataControll = (req, resp) =>
+  resp.write("This is Data Controller Function");
+resp.end();
