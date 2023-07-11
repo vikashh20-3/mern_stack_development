8fb8e85339eb0ad41573404854d30e2a851fa819
vikashh20-3 <vvikashrao203@gmail.com>
2023-07-11 12:58:33 +0530

    append file 12:58 11Jul

--- filesystem.js
+++ filesystem.js
@@ -6,12 +6,26 @@ console.log("File Name", __filename); //to show the file name
 // Create files in a folder using loop
 
 const path = require("path"); // give us the path 
-const dirPath = path.join(__dirname, "fsfiles"); //It Provides us the path for our folders
+const dirPath = path.join(__dirname, "fsfiles"); //It Provides us the path for our fs files folder
+const dirCrudPath=path.join(__dirname,"crud-files")//It Provides us the path for our crud-files folder
+const crudpath=`${dirCrudPath}/crudfile.txt`;
 
-fs.writeFileSync(dirPath + "/file.txt", "demo for folder");
+// fs.writeFileSync(dirPath + "/file.txt", "demo for folder");
 
 //file  creation with loop
 
-for (i = 2; i < 5; i++) {
-  fs.writeFileSync(dirPath + `/demofiles${i}`, "demo text");
-}
+// for (i = 2; i < 5; i++) {
+//   fs.writeFileSync(dirPath + `/demofiles${i}`, "demo text");
+// }
+
+
+fs.writeFileSync( `${dirCrudPath}/crudfile.txt` , "this is only ")
+fs.appendFile(crudpath,"the crud files operation",(err)=>{ // USE appendFile to update exisiting file
+  if(err){
+
+    console.log(err);
+  } 
+  else{
+    console.log("No Error Detected")
+  }
+})
