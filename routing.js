213b7301304b50732f46557faa3fa27afd905611
vikashh20-3 <vvikashrao203@gmail.com>
2023-07-13 11:25:08 +0530

    ejs engine routing 11:25 13Jul

--- routing.js
+++ routing.js
@@ -1,34 +1,42 @@
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-const express=require('express')
-const app=express();
-const publicPath=path.join(__dirname,'public');
-console.log(publicPath);
+const app = express();
+const publicPath=path.join(__dirname,'public')
 
-// app.use(express.static(publicPath));// this method is used to load static pages with extension in url
-// app.get('',(req,resp)=>{
-//     resp.send("This is homepage of our application")
-// })
+app.set('view engine','ejs');
 
-// app.get('/about',(req,resp)=>{
-//     //to write in browser        /about/?name="vikash"
-//     resp.send("This is About Page of our application "+req.query.name)
-// })
-
-// app.get('/contact',(req,resp)=>{
-//     resp.send("This is Contact Page of our application")
-// })
-
-app.get('',(req,resp)=>{
+app.get('',(_,resp)=>{
     resp.sendFile(`${publicPath}/index.html`)
-})
+});
 
 app.get('/about',(req,resp)=>{
+    //to write in browser        /about/?name="vikash"
+    resp.send("This is About Page of our application "+req.query.name)
+})
+
+
+app.get('/profile',(_,resp)=>{
+    const user={
+        name:'vikku',
+        email:'viku@gmail.com',
+        country:'india'
+    }
+   resp.render('profile',{user})
+});
+
+app.get('/contact',(_,resp)=>{
     resp.sendFile(`${publicPath}/about.html`)
-})
-app.get('*',(req,resp)=>{
-        resp.sendFile(`${publicPath}/404.html`)
-})
+});
+
+app.get('/help',(_,resp)=>{
+    resp.sendFile(`${publicPath}/help.html`)
+});
+
+app.get('*',(_,resp)=>{
+    resp.sendFile(`${publicPath}/nopage.html`)
+});
+
+app.listen(5000);
 
 
-app.listen(5000)
