5176fe067713216e829e483f0fe12bc1fe5e701e
vikashh20-3 <vvikashrao203@gmail.com>
2023-07-13 12:50:02 +0530

    Application level-Middleware

--- input.js
+++ input.js
@@ -3,11 +3,11 @@ console.log(process.argv);
 
 const fs = require("fs");
 let input = process.argv;
-
+// fs.writeFileSync(input[2],input[3]);
 if (input[2] === "add") {
-  fs.writeFileSync(input[3], input[4]);
+  fs.writeFileSync(input[2], input[3]);
 } else if (input[2] === "remove") {
-  fs.unlinkSync(input[3]); //this will delete the file
+  fs.unlinkSync(input[2]); //this will delete the file
 } else {
   console.log("Invalid ");
 }
--- routing.js
+++ routing.js
@@ -6,6 +6,21 @@ const publicPath=path.join(__dirname,'public')
 
 app.set('view engine','ejs');
 
+//THIS IS APPLICATION LEVEL MIDDLEWARE
+const reqFilter=(req,resp,next)=>{
+    console.log(reqFilter);
+    if(!req.query.age){
+        resp.send("Enter you age")
+    }else if (req.query.age<18){
+        resp.send("You are not allowed to see this page ")
+    }else{
+
+        next();
+    }
+    
+}
+app.use(reqFilter)
+
 app.get('',(_,resp)=>{
     resp.sendFile(`${publicPath}/index.html`)
 });
